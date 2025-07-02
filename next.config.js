06f5f8951ffada5a2fbc2610eaa5a51d336a9206
sdffdssf <poiooiiyutyrssfsds@gmail.com>
2025-07-02 03:19:12 -0700

    Add files via upload

--- next.config.js
+++ next.config.js
@@ -1,10 +1,6 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-  output: 'export',
-  distDir: 'out',
-  trailingSlash: true,
   images: {
-    unoptimized: true,
     domains: [
       "source.unsplash.com",
       "images.unsplash.com",
